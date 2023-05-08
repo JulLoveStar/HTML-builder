2f40ba5fc6de628296dafd44545f162aff515755
Macbook <jul.love.star@gmail.com>
2023-05-08 11:28:03 +0400

    add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require("fs");
+const path = require("path");
 
+const textPathCopy = path.join(__dirname, "files-copy");
+const textPath = path.join(__dirname, "files");
+
+function copyDir() {
+
+  fs.stat(textPathCopy, (err, stats) => {
+    if (err) {
+      fs.mkdir(textPathCopy, (err) => {
+        if (err) {
+          throw err;
+        }
+        fs.readdir(textPath,(err, files)=>{
+            if (err) {
+                throw err;
+              }
+            for(let i=0; i<files.length; i++){
+                let oldFile=path.join(textPath,files[i])
+                let newFile=path.join(textPathCopy,files[i])
+                fs.copyFile(oldFile,newFile,(err)=>{
+                    if(err) throw err;
+                })  
+            }
+            
+        })
+      });
+    }else{console.log('папка уже существует')}
+  });
+}
+
+copyDir();
